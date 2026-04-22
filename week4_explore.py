10c99f8a350612a000e260b9480d32ed86f11ec8
steodhiambo <stephenoginga6@gmail.com>
2026-04-23 00:34:37 +0300

    Add Week 4 dataset exploration script

--- week4_explore.py
+++ week4_explore.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-xl = pd.ExcelFile('Travler Week 4 Paid Advertising Dataset.xlsx')
+xl = pd.ExcelFile('Travler Week 4 Paid Ads and Budget Dataset.xlsx')
 print("SHEETS:", xl.sheet_names)
 print()
 for sheet in xl.sheet_names:
